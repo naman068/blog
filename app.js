6562e289e97bf97efcde838a209c1d88305da4cb
Naman <namank682003@gmail.com>
2022-09-22 01:27:51 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express= require("express")
+const path = require ("path")
+const app = express()
+const port = 80
+
+app.use('/css',express.static(__dirname + '/css'));
+app.use('/img',express.static(__dirname + '/img'));
+
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/templates/index.html')
+})
+
+app.get('/blogs', (req,res)=>{
+    res.sendFile(__dirname + '/templates/blogs.html')
+})
+
+app.get('/contact', (req,res)=>{
+    res.sendFile(__dirname + '/templates/contact.html')
+})
+
+app.post('/contact', (req,res)=>{
+    res.sendFile(__dirname + '/templates/contact.html')
+})
+
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port ${port}`);
+});
